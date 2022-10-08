9837a2ee77efa690134b68248c3138a8f0f75146
Karthik <karthik29102001@gmail.com>
2022-10-08 18:40:33 +0530

    added gallery and faq section

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+document.querySelectorAll(".image-container img").forEach((image) => {
+  image.onclick = () => {
+    document.querySelector(".pop-image").style.display = "block";
+    document.querySelector(".pop-image img").src = image.getAttribute("src");
+  };
+});
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+document.querySelector(".pop-image span").onclick = () => {
+  document.querySelector(".pop-image").style.display = "none";
+};
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
