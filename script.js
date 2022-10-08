cd28c94d838497d8df4da0be4066b8fd40adaf96
Balakrishna K <kbalakrishna017@gmail.com>
2022-10-09 01:33:40 +0530

    testimonial

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ function toggleAccordion() {
 
 items.forEach((item) => item.addEventListener("click", toggleAccordion));
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
